9d1d7b6c983f534b2323137317372bd36e7b900b
Nevio Carcangiu <nevio@carcangiu.net>
2025-11-03 15:14:27 +0100

    go-1-ex-3 finished

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -22,5 +22,10 @@ func main() {
 
 	// TODO: how to write the output into eyes.txt and dice.log?
 
+	//$go run go-1-ex-3/main.go >out.txt 2>&1
+	//$cat out.txt
+	//$the dice shows 1 eyes
+	//$the dice was rolled at 2025-11-03 15:12:07.4377596 +0100 CET m=+0.000512401
+
 	// go run ex3/main.go TODO
 }
