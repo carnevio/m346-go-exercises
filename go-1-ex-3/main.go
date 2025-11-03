a58f1067d105e2b8d8f4c66aea3a6a60c0408fcf
Nevio Carcangiu <nevio@carcangiu.net>
2025-11-03 15:07:11 +0100

    go-1-ex-3 finished

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -4,18 +4,23 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"os"
 )
 
 func main() {
-	var eyes = rand.Intn(5) + 1
-	var when = time.Now()
+	rand.Seed(time.Now().UnixNano()) // Zufallszahlengenerator initialisieren
+
+	eyes := rand.Intn(6) + 1
+	when := time.Now()
 
 	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	fmt.Fprintln(os.Stdout, "the dice shows", eyes, "eyes")
 
 	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	fmt.Fprintln(os.Stderr, "the dice was rolled at", when)
+	
 
 	// TODO: how to write the output into eyes.txt and dice.log?
+
 	// go run ex3/main.go TODO
 }
