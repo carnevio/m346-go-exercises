15b2b5a3b2914d3ce8c3f63c5aabfae12aed51d0
Nevio Carcangiu <nevio@carcangiu.net>
2025-11-03 14:39:07 +0100

    go-1-ex-1 finished

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,20 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName = "Nevio"
+	var lastName  = "Carcangiu"
+
+	var dayOfBirth = 1
+	var monthOfBirth = 8
+	var yearOfBirth = 2008
+
+	var numberOfSiblings = 1
+
+	var heightInMeters = 1.80
+
+	var zodiacSign = '\u264C'
+
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
