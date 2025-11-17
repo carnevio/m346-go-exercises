8e540852c1f4dc3566b299458466346cd488a515
Nevio Carcangiu <nevio@carcangiu.net>
2025-11-17 14:36:07 +0100

    Go2 excercise 1 finished

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -3,26 +3,40 @@ package main
 import "fmt"
 
 type FullName struct {
-	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
-// TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   int
+	Month int
+	Year  int
+}
 
 type Profile struct {
-	// TODO: embed full name and birth date information
+	FullName
+	BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
 
 func main() {
 	var me = Profile{
-		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		FullName: FullName{
+			FirstName: "Nevio",
+			LastName:  "Carcangiu",
+		},
+		BirthDate: BirthDate{
+			Day:   1,
+			Month: 8,
+			Year:  2008,
+		},
+		NumberOfSiblings: 1,
+		ZodiacSign:       '\u264C',
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
-	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
