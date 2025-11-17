c7b7eb46f3d379280166476fc1236e4730d8b798
Nevio Carcangiu <nevio@carcangiu.net>
2025-11-17 15:00:11 +0100

    go-2-ex-3 finished

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,13 +4,22 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
+	modules := map[int]string{
+		104: "Programmieren I",
+		117: "Datenbanken",
+		346: "Cloud-Lösungen konzipieren und realisieren",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
 	// TODO: delete one
+	delete(modules, 117)
 	// TODO: add one
+	modules[210] = "Netzwerk"
 	// TODO: replace one
+	modules[104] = "Programmieren in Go"
+
 	fmt.Println(modules)
 }
