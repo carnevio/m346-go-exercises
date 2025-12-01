f0186e7f6c7dd6757fbe7e906e1b74fda62eb6a5
Nevio Carcangiu <nevio@carcangiu.net>
2025-12-01 14:19:51 +0100

    go-4-ex-1 finished

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,17 @@
 package main
 
+import "fmt"
+
 // TODO: implement the function computeGrade
+func computeGrade(gotPoints float32, maxPoints float32) {
+	y := (gotPoints/maxPoints)*5 + 1
+	fmt.Println(y)
+
+}
 
 func main() {
 	// TODO: call the function computeGrade
+	computeGrade(17.5, 28.0) // 4.125
+	computeGrade(20.5, 30.0) // 4.416667
+	computeGrade(28.0, 28.0) // 6
 }
